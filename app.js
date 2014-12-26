a541754d1bfd02f6b067bc58fab5ea08c58a6e15
Nicholas Klunder <nklunder@gmail.com>
2014-12-25 17:10:08 -0800

    move CSS to external file, make it less ugly

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var cache = {};
 
-$(".ajax-tabs li").on("click", function () {
-  var $displayArea = $(".display-area"),
-      page = $(this).attr("class");
-      
-      console.log(page);
+$('.ajax-tabs li').on('click', function () {
+  var $displayArea = $('.display-area'),
+      page = $(this).data('tab');
+
+  $(this).toggleClass('selected').siblings().removeClass('selected');
 
   if (cache[page]) {
     $displayArea.html(cache[page]);
   } else {
-    $.get("pages/" + page + ".html", function (data) {
+    $.get('pages/' + page + '.html', function (data) {
       $displayArea.html(data);
       cache[page] = data;
     });
